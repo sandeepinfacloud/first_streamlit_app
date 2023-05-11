9b6792b9c6b4e3b5bdee084b8425e89adedbaa78
sandeepinfacloud <133200931+sandeepinfacloud@users.noreply.github.com>
2023-05-11 13:38:46 +0530

    Update streamlit_app.py
    added a line to show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,5 @@ streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
-
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
