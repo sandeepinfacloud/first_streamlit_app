421f3021060d7429f4b5913825a09cdd09a1f834
sandeepinfacloud <133200931+sandeepinfacloud@users.noreply.github.com>
2023-05-11 23:14:55 +0530

    Update streamlit_app.py
    try exception block

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from urllib.error import URLerror
+from urllib.error import URLerror
 
 streamlit.title('My Mom\'s New healthy Dinner 🥣 🥗 🐔 🥑🍞')
 
@@ -34,20 +34,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if no fruit_choice:
+  streamlit.error("please select a fruit to get information")
+  else:
 streamlit.write('The user entered ', fruit_choice)
-
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
-
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.stop()
+except URLError as e:
+  streamlit.stop()
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
