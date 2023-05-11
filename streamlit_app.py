2d7575b01f65351a0c4b02313d62ed8a528ed9df
sandeepinfacloud <133200931+sandeepinfacloud@users.noreply.github.com>
2023-05-11 23:22:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 #import requests
 try:  
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
