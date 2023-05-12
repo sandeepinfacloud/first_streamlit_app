9116ab02209e6e5c8ff55aeaf3d81b73c069d799
sandeepinfacloud <133200931+sandeepinfacloud@users.noreply.github.com>
2023-05-12 11:12:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,13 +54,13 @@ except URLError as e:
         streamlit.error()
         #streamlit.stop()
     
-#import snowflake.connector
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_rows = my_cur.fetchall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_rows)
+import snowflake.connector
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ my_cur = my_cnx.cursor()
+ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+ my_data_rows = my_cur.fetchall()
+ streamlit.header("The fruit load list contains:")
+ streamlit.dataframe(my_data_rows)
 
 streamlit.header("View Our Fruit List - Add Your Favorites!:")
 
